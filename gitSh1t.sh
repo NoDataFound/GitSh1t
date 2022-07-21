3fffd83ecc914fb13355c08d9c90749e05aeafab
Corian (Cory) Kennedy <clkennedy@gmail.com>
2022-07-21 16:07:33 -0500

    Update gitSh1t.sh

--- gitSh1t.sh
+++ gitSh1t.sh
@@ -18,7 +18,7 @@ echo "
                                 ||---23 | 💩
                                 ||     || "
 echo ""
-file="loot.txt"
+file="/Users/0xdeadbeef/git.txt"
 n=1
 n=$((n+1))
 mkdir "loot"
@@ -28,10 +28,10 @@ while read line; do
     gituser=$line
     mkdir $gituser
     cd $gituser/
+    curl -s "https://api.github.com/users/$gituser/repos?per_page=1000" | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
     cd ..
 done < $file
 echo "Line No. $n : $line"
-curl -s "https://api.github.com/users/NoDataFound/repos?per_page=1000" | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
 for r in `find . -path ./git -prune -o -type d -mindepth 1 -maxdepth 1` ; do
     `gitleaks detect --source "$r" -v -l debug -r thisreportissh1t.json -f json > $r/sh1t.json`
 done
